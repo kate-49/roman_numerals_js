232e4fc939f36d24a4cad61cceb9d22da6a4cee0
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-02-11 11:44:56 +0000

    failing test - throwing errors for incorrect input type

--- spec/numToRoman.test.js
+++ spec/numToRoman.test.js
@@ -8,10 +8,14 @@ describe('NumToRoman', function() {
   });
 
   it('can return a string when given an integer', function() {
-    converter = new NumToRoman();
     expect(converter.run(10)).toEqual("X");
   });
   
+  it('will raise errors when not given correct input', function() {
+    expect(converter.run(10.00)).toThrow(new Error("Incorrect input type"));
+    expect(converter.run("test string")).toThrow(new Error("Incorrect input type"));
+    expect(converter.run(true)).toThrow(new Error("Incorrect input type"));
+  });
 })
 
 
