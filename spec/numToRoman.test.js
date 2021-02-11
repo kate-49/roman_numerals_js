769faed1b660e42750cb032a12e60da80769abd3
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-02-11 14:49:25 +0000

    missed off previous commit

--- spec/numToRoman.test.js
+++ spec/numToRoman.test.js
@@ -12,6 +12,17 @@ describe('NumToRoman', function() {
     expect(() => {converter.run("test string")}).toThrowError(TypeError('Wrong input type'))
     expect(() => {converter.run(true)}).toThrowError(TypeError('Wrong input type'))
   });
+
+  it('will return the correct value for main types of number when given seperatley', () => {
+    expect(converter.run(1)).toEqual("I")
+    expect(converter.run(5)).toEqual("V")
+    expect(converter.run(10)).toEqual("X")
+    expect(converter.run(50)).toEqual("L")
+    expect(converter.run(100)).toEqual("C")
+    expect(converter.run(500)).toEqual("D")
+    expect(converter.run(1000)).toEqual("M")
+  })
+
 })
 
 
