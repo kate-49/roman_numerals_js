016c79b308a0dd7e6d4334e7d023a492a780b7d5
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-02-19 14:27:52 +0000

    refactored check Hundreds test

--- spec/checkHundreds.test.js
+++ spec/checkHundreds.test.js
@@ -2,13 +2,18 @@ const NumToRoman = require('../src/numToRoman.js');
 
 describe('checkHundreds', function() {
 
-  it('will return the correct value for main types of number when given seperatley', () => {
+  it('will map input according to valuemap and add this to answer', () => {
    
     converter = new NumToRoman(100);
-    expect(converter.run()).toEqual("C")
+    converter.checkHundreds()
+    expect(converter.answer).toEqual("C")
 
     converter = new NumToRoman(500);
-    expect(converter.run()).toEqual("D")
+    converter.checkHundreds()
+    expect(converter.answer).toEqual("D")
         
+    converter = new NumToRoman(900);
+    converter.checkHundreds()
+    expect(converter.answer).toEqual("CM")
   });
 })
