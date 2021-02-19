0f8756936908025ac932506bc6dd254cc6662f71
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-02-19 15:00:11 +0000

    refactored checkInputvalid

--- spec/checkInputValid.test.js
+++ spec/checkInputValid.test.js
@@ -4,20 +4,20 @@ describe('checkInputValid', function() {
 
     it('will only accept integer input', () => {
         converter = new NumToRoman(10);
-        expect(() => {converter.run()}).not.toThrowError(Error('Wrong input type'))
+        expect(() => {converter.checkInputValid()}).not.toThrowError(Error('Wrong input type'))
     
         converter = new NumToRoman("test string");
-        expect(() => {converter.run()}).toThrowError(Error('Wrong input type'))
+        expect(() => {converter.checkInputValid()}).toThrowError(Error('Wrong input type'))
     
         converter = new NumToRoman(true);
-        expect(() => {converter.run()}).toThrowError(Error('Wrong input type'))
+        expect(() => {converter.checkInputValid()}).toThrowError(Error('Wrong input type'))
       });
 
       it('will not accept integers that are too large', () => {
         converter = new NumToRoman(1000);
-        expect(() => {converter.run()}).toThrowError(Error('Input too large, input must be less than 1000'))
+        expect(() => {converter.checkInputValid()}).toThrowError(Error('Input too large, input must be less than 1000'))
     
         converter = new NumToRoman(3651);
-        expect(() => {converter.run()}).toThrowError(Error('Input too large, input must be less than 1000'))
+        expect(() => {converter.checkInputValid()}).toThrowError(Error('Input too large, input must be less than 1000'))
       });
 });
