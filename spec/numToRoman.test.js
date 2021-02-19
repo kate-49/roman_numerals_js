e97fe40345c8504d8a3a3be56eb3a8bdf66062db
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-02-19 14:06:18 +0000

    refactored to move number into constructor to remove dependencies

--- spec/numToRoman.test.js
+++ spec/numToRoman.test.js
@@ -4,50 +4,50 @@ describe('NumToRoman', function() {
   var converter;
 
   it('will only accept integer input', () => {
-    converter = new NumToRoman();
-    expect(() => {converter.run(10)}).not.toThrowError(Error('Wrong input type'))
+    converter = new NumToRoman(10);
+    expect(() => {converter.run()}).not.toThrowError(Error('Wrong input type'))
 
-    converter = new NumToRoman();
-    expect(() => {converter.run("test string")}).toThrowError(Error('Wrong input type'))
+    converter = new NumToRoman("test string");
+    expect(() => {converter.run()}).toThrowError(Error('Wrong input type'))
 
-    converter = new NumToRoman();
-    expect(() => {converter.run(true)}).toThrowError(Error('Wrong input type'))
+    converter = new NumToRoman(true);
+    expect(() => {converter.run()}).toThrowError(Error('Wrong input type'))
   });
 
   it('will return the correct value for main types of number when given seperatley', () => {
    
-    converter = new NumToRoman();
-    expect(converter.run(1)).toEqual("I")
+    converter = new NumToRoman(1);
+    expect(converter.run()).toEqual("I")
     
-    converter = new NumToRoman();
-    expect(converter.run(5)).toEqual("V")
+    converter = new NumToRoman(5);
+    expect(converter.run()).toEqual("V")
     
-    converter = new NumToRoman();
-    expect(converter.run(10)).toEqual("X")
+    converter = new NumToRoman(10);
+    expect(converter.run()).toEqual("X")
 
-    converter = new NumToRoman();
-    expect(converter.run(50)).toEqual("L")
+    converter = new NumToRoman(50);
+    expect(converter.run()).toEqual("L")
 
-    converter = new NumToRoman();
-    expect(converter.run(100)).toEqual("C")
+    converter = new NumToRoman(100);
+    expect(converter.run()).toEqual("C")
 
-    converter = new NumToRoman();
-    expect(converter.run(500)).toEqual("D")
+    converter = new NumToRoman(500);
+    expect(converter.run()).toEqual("D")
         
   });
 
   it('can handle complex numbers', () => {
-    converter = new NumToRoman();
-    expect(converter.run(15)).toEqual("XV")
+    converter = new NumToRoman(5);
+    expect(converter.run()).toEqual("V")
 
-    converter = new NumToRoman();
-    expect(converter.run(159)).toEqual("CLIX")
+    converter = new NumToRoman(159);
+    expect(converter.run()).toEqual("CLIX")
 
-    converter = new NumToRoman();
-    expect(converter.run(890)).toEqual("DCCCXC")
+    converter = new NumToRoman(890);
+    expect(converter.run()).toEqual("DCCCXC")
 
-    converter = new NumToRoman();
-    expect(converter.run(101)).toEqual("CI")
+    converter = new NumToRoman(101);
+    expect(converter.run()).toEqual("CI")
   });
 
 })
--- src/numToRoman.js
+++ src/numToRoman.js
@@ -1,39 +1,48 @@
 
 class NumToRoman {
   
-  constructor() {
+  constructor(number) {
+    this.number = number;
     this.onesValuemap = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
     this.tensValuemap = ['X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
     this.hundredsValuemap = ['C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
     this.answer = "";
   }
 
-  checkInputValid(number) {
-    if (Number.isInteger(number) != true) {
+  checkInputValid() {
+    if (Number.isInteger(this.number) != true) {
       throw new Error('Wrong input type');
-  }
+    }
   }
 
-  run(number) {
-    this.checkInputValid(number)
+  checkHundreds() {
+    var hundreds = Math.floor((this.number/100))
+    var value = this.hundredsValuemap[hundreds-1]
+    this.answer += value
+    this.number -= (hundreds * 100)
+  }
 
-    if (number > 99) {
-      var hundreds = Math.floor((number/100))
-      var value = this.hundredsValuemap[hundreds-1]
-      this.answer += value
-      number -= (hundreds * 100)
+  checkTens() {
+    var tens = Math.floor((this.number / 10))
+    var value = this.tensValuemap[tens-1]      
+    this.answer += value
+    this.number -= (tens * 10)
+  }
+  
+
+  run() {
+    this.checkInputValid()
+
+    if (this.number > 99) {
+      this.checkHundreds()
     }
 
-    if (number > 9) {
-      var tens = Math.floor((number / 10))
-      var value = this.tensValuemap[tens-1]      
-      this.answer += value
-      number -= (tens * 10)
-    
+    if (this.number > 9) {
+      this.checkTens()
     }
     
-    if (number) {
-      var value = this.onesValuemap[number-1]
+    if (this.number) {
+      var value = this.onesValuemap[this.number-1]
       this.answer += value
     }
 
