0e1ce899e7d9a6a112a03fab136f028449dd9e78
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-02-19 15:02:00 +0000

    added checks that errors thrown when whole program is run

--- spec/numToRoman.test.js
+++ spec/numToRoman.test.js
@@ -16,6 +16,25 @@ describe('NumToRoman', function() {
     expect(converter.run()).toEqual("CI")
   });
 
+  it('will only accept integer input', () => {
+    converter = new NumToRoman(100);
+    expect(() => {converter.run()}).not.toThrowError(Error('Wrong input type'))
+
+    converter = new NumToRoman("test string two");
+    expect(() => {converter.run()}).toThrowError(Error('Wrong input type'))
+
+    converter = new NumToRoman(false);
+    expect(() => {converter.run()}).toThrowError(Error('Wrong input type'))
+  });
+
+  it('will not accept integers that are too large', () => {
+    converter = new NumToRoman(6666);
+    expect(() => {converter.run()}).toThrowError(Error('Input too large, input must be less than 1000'))
+
+    converter = new NumToRoman(5432);
+    expect(() => {converter.run()}).toThrowError(Error('Input too large, input must be less than 1000'))
+  });
+  
 })
 
 
