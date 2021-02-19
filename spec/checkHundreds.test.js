bfdc7aa4d96a7577939ed6add1036115045e0da6
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-02-19 14:39:57 +0000

    refactored hundreds more in line with tens refactoring

--- spec/checkHundreds.test.js
+++ spec/checkHundreds.test.js
@@ -4,16 +4,19 @@ describe('checkHundreds', function() {
 
   it('will map input according to valuemap and add this to answer', () => {
    
-    converter = new NumToRoman(100);
+    converter = new NumToRoman(161);
     converter.checkHundreds()
     expect(converter.answer).toEqual("C")
+    expect(converter.number).toEqual(61)
 
-    converter = new NumToRoman(500);
+    converter = new NumToRoman(599);
     converter.checkHundreds()
     expect(converter.answer).toEqual("D")
+    expect(converter.number).toEqual(99)
         
     converter = new NumToRoman(900);
     converter.checkHundreds()
     expect(converter.answer).toEqual("CM")
+    expect(converter.number).toEqual(0)
   });
 })
